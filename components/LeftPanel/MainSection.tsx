f6b5b36cd091383612bdeebe7c9b727202efa81e
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2024-09-17 01:17:24 -0700

    animate the cube

--- components/LeftPanel/MainSection.tsx
+++ components/LeftPanel/MainSection.tsx
@@ -8,6 +8,9 @@ export default function MainSection() {
   return (
     <section id="canvas-container" className="w-full h-full">
       <Canvas>
+        <ambientLight />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
         <RotatingCube />
       </Canvas>
     </section>
